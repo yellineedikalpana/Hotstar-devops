57aeb7e815c4d2ff37c6d5d359d5e3141afc80b3
Naresh916 <Nareshtullibilli666@gmail.com>
2026-03-26 19:39:47 +0530

    Create SearchBar.js

---
+++ hotstar/src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form className="search-bar" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
